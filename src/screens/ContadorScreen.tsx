209120335348bfa74cd5aca42bac10cd71e8ac70
Rafael Escobar Gutierrez <Rafael.EG.code@gmail.com>
2022-09-30 21:07:22 -0500

    StyleSheet

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -1,20 +1,13 @@
 import React, { useState } from 'react'
-import { Text, View, Button, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 
 export const ContadorScreen = () => {
 
     const [conteo, setConteo] = useState(10);
 
-  return (
-    <View style={{
-        flex: 1,
-        justifyContent: 'center',
-    }}>
-        <Text style={{
-            fontSize: 30,
-            color: 'black',
-            textAlign: 'center',
-        }}>Contador: {conteo}</Text>
+  return ( 
+    <View style={ style.conainer }>
+        <Text style={ style.etiqueta_con }>Contador: {conteo}</Text>
         {/* <Button 
             title='+'
             onPress={ () => setConteo(conteo+1) } 
@@ -23,17 +16,33 @@ export const ContadorScreen = () => {
         <TouchableOpacity
             onPress={ () => setConteo( conteo + 1 ) }
         >
-            <View style={{
-                backgroundColor: 'aqua',
-                borderRadius: 100,
-            }}>
-                <Text style={{
-                    fontSize: 20,
-                    color: 'black',
-                    textAlign: 'center',
-                }}>+1</Text>
+            <View style={ style.btn }>
+                <Text style={ style.btn_font }>+1</Text>
             </View>
         </TouchableOpacity>
     </View>
   )
 }
+
+
+const style = StyleSheet.create({
+    conainer: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    etiqueta_con: {
+        fontSize: 30,
+        color: 'black',
+        textAlign: 'center',
+    },
+    btn: {
+        backgroundColor: 'aqua',
+        borderRadius: 100,
+    },
+    btn_font: {
+        fontSize: 20,
+        color: 'black',
+        textAlign: 'center',
+    }
+
+})
